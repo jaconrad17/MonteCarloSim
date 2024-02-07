2a6be875d37951b4c51d499b2429d0f7712f84e5
Jonathan Conrad <jaconrad17@gmail.com>
2024-02-06 23:11:02 -0800

    Update PrimaryGeneratorAction.hh

--- include/PrimaryGeneratorAction.hh
+++ include/PrimaryGeneratorAction.hh
@@ -57,69 +57,67 @@ enum { EPGA_BEAM, EPGA_ROOT};
 class PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
 {
 public:
-    PrimaryGeneratorAction();
-    virtual ~PrimaryGeneratorAction();
+PrimaryGeneratorAction();
+virtual ~PrimaryGeneratorAction();
     
-    virtual void GeneratePrimaries(G4Event*);
-    void SetUpROOTInput (TString filename);
+void GeneratePrimaries(G4Event*);
+void SetUpROOTInput (TString filename);
     
-    void SetMode(G4int mo) {fMode = mo;}
-    void SetNEvents(G4int nev) {fNevents = nev;}
+void SetMode(G4int mo) {fMode = mo;}
+void SetNEvents(G4int nev) {fNevents = nev;}
     
-    G4ParticleGun* GetParticleGun() { return fParticleGun; };
+G4ParticleGun* GetParticleGun() { return fParticleGun; };
     
-    G4int GetMode() {return fMode;}
-    G4int GetNEvents() {return fNevents;}
+G4int GetMode() {return fMode;}
+G4int GetNEvents() {return fNevents;}
     
-    G4double GetWeight() {return (G4double)fWeight;}
-    G4int GetFlag() {return fFlag;}
-    G4int GetNPrimaryParticles() {return fNPrimParticles;}
-    G4ThreeVector GetVertex(G4int i) {return G4ThreeVector(fVx[i], fVy[i], fVz[i]);}
-    G4ThreeVector GetDirection(G4int i) {return G4ThreeVector(fPx[i], fPy[i], fPz[i]);}
-    G4double GetEnergy(G4int i) {return fE[i];}
-    G4ParticleDefinition* GetPrimPDef(G4int i){return fPDefinition[i];}
+G4double GetWeight() {return (G4double)fWeight;}
+G4int GetFlag() {return fFlag;}
+G4int GetNPrimaryParticles() {return fNPrimParticles;}
+G4ThreeVector GetVertex(G4int i) {return G4ThreeVector(fVx[i], fVy[i], fVz[i]);}
+G4ThreeVector GetDirection(G4int i) {return G4ThreeVector(fPx[i], fPy[i], fPz[i]);}
+G4double GetEnergy(G4int i) {return fE[i];}
+G4ParticleDefinition* GetPrimPDef(G4int i){return fPDefinition[i];}
     
-    void SetOptPhotonPolar();
-    void SetOptPhotonPolar(G4double);
-    void SetRandomDirection(G4bool val = true);
-    G4bool GetPolarized() { return fPolarized; };
-    G4double GetPolarization() { return fPolarization; }
+void SetOptPhotonPolar();
+void SetOptPhotonPolar(G4double);
+G4bool GetPolarized() { return fPolarized; };
+G4double GetPolarization() { return fPolarization; }
     
 private:
-    PrimaryGeneratorMessenger* fGunMessenger;
-    G4ParticleGun* fParticleGun;
-    G4GeneralParticleSource* fParticleSource;
-    G4ParticleTable* fParticleTable;
+PrimaryGeneratorMessenger* fGunMessenger;
+G4ParticleGun* fParticleGun;
+G4GeneralParticleSource* fParticleSource;
+G4ParticleTable* fParticleTable;
+
+G4int fMode;
+G4int fNevents;
+G4int fEvent;
     
-    G4int fMode;
-    G4int fNevents;
-    G4int fEvent;
+G4bool fPolarized;
+G4double fPolarization;
     
-    G4bool fRandomDirection;
-    G4bool fPolarized;
-    G4double fPolarization;
+static const G4int fMaxprim = 50;
     
-    static const G4int fMaxprim = 50;
+G4ParticleDefinitions* fPDefinition[fMaxprim];
     
-    G4ParticleDefinitions* fPDefinition[fMaxprim];
+TFile* fGenFile;
+TTree* fGenTree;
+Int_t fNGenBranches;
     
-    TFile* fGenFile;
-    TTree* fGenTree;
-    Int_t fNGenBranches;
+Int_t fNPrimParticles;
+Double_t fWeight;
+Int_t fFlag;
     
-    Int_t fNPrimParticles;
-    Double_t fWeight;
-    Int_t fFlag;
+Int_t fPDG[fMaxprim];
     
-    Int_t fPDG[fMaxprim];
-    
-    Double_t fVx[fMaxprim];
-    Double_t fVy[fMaxprim];
-    Double_t fVz[fMaxprim];
-    Double_t fPx[fMaxprim];
-    Double_t fPy[fMaxprim];
-    Double_t fPz[fMaxprim];
-    Double_t fE[fMaxprim];
+Double_t fVx[fMaxprim];
+Double_t fVy[fMaxprim];
+Double_t fVz[fMaxprim];
+Double_t fPx[fMaxprim];
+Double_t fPy[fMaxprim];
+Double_t fPz[fMaxprim];
+Double_t fE[fMaxprim];
 };
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
